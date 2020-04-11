73ff361d7ea4c6f1257005bd9c21afd250a34609
Guillaume Gillet <guillaume.gillet@etu.utc.fr>
2020-04-11 18:23:44 +0200

    Update MessageReceptor.java

--- Devoir1/src/clientPackage/MessageReceptor.java
+++ Devoir1/src/clientPackage/MessageReceptor.java
@@ -7,7 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Thread pour le client qui reçoit les messages de ThreadServer 
+ * Thread pour le client qui recoit les messages de ThreadServer 
  */
 public class MessageReceptor extends Thread {
 
@@ -40,15 +40,15 @@ public class MessageReceptor extends Thread {
 		String messageRecu = "Exemple";
 		boolean condition = true;
 		try {
-			// Tant que l'utilisateur n'a pas envoyé exit, on lit les messages venant des autres utilisateurs
+			// Tant que l'utilisateur n'a pas envoye exit, on lit les messages venant des autres utilisateurs
 			while (condition) {
 				messageRecu = (String) ois.readObject();
 				if (!messageRecu.startsWith(mainclient.utilisateur.getPseudo()) || !messageRecu.contains(mainclient.utilisateur.getPseudo()))
-					System.out.println(messageRecu); // On affiche le message uniquement si ce n'est pas le même utilisateur qui l'a envoyé
+					System.out.println(messageRecu); // On affiche le message uniquement si ce n'est pas le mme utilisateur qui l'a envoy
 				if (messageRecu.startsWith("exit")) condition = false;
-				// Si exit a été envoyé, il faut déconnecter le client et donc arrêter ses threads
+				// Si exit a ete envoye, il faut deconnecter le client et donc arreter ses threads
 				// Par manque de temps, nous ne l'avons pas fait
-				// Il suffit de vérifier si le "exit" reçu vient du même utilisateur que cette instance du thread
+				// Il suffit de verifier si le "exit" vient du meme utilisateur que cette instance du thread
 			}
 		} catch (ClassNotFoundException e) {
 			Logger.getLogger(MessageReceptor.class.getName()).log(Level.SEVERE, null, e);
