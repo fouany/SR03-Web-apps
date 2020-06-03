1adb16af38a43420b867a85a390ed17c1e7fbaae
Antoine Herkens <antoine.herkens@etu.utc.fr>
2020-06-03 10:53:45 +0200

    delete

--- Devoir2/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-    ini_set('display_errors', 0);
-    ini_set('session.cookie_httponly', 1); 
-    ini_set('session.use_only_cookies', 1);
-	//ce fichier permet de bloquer le cookie pour éviter qu'il soit récupérable par un script
-?>
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # SR03
 
-Guillaume GILLET et Antoine HERKENS
 
+
+Guillaume GILLET et  Antoine HERKENS
 Pseudos picasoft : guillaume_1507 et antoine_herkens
-
-TD Mardi 16h30
