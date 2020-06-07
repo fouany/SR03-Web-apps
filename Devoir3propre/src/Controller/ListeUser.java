fcf54a464b9fadb91df7666057c1c0885fd69742
Antoine Herkens <antoine.herkens@etu.utc.fr>
2020-06-07 17:51:42 +0200

    Update ListeUser.java

--- Devoir3propre/src/Controller/ListeUser.java
+++ Devoir3propre/src/Controller/ListeUser.java
@@ -18,6 +18,7 @@ import Model.User;
  */
 @WebServlet("/ListeUser")
 public class ListeUser extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -25,24 +26,19 @@ public class ListeUser extends HttpServlet {
      */
     public ListeUser() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		try {
 			request.setAttribute("user", User.FindAll());
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	    RequestDispatcher rd = request.getRequestDispatcher("AfficheUsers.jsp");
