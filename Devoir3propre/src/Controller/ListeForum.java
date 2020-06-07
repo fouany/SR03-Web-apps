7416d74c67ef980ad72a064db095f3f0f3042357
Antoine Herkens <antoine.herkens@etu.utc.fr>
2020-06-07 17:51:15 +0200

    Update ListeForum.java

--- Devoir3propre/src/Controller/ListeForum.java
+++ Devoir3propre/src/Controller/ListeForum.java
@@ -24,32 +24,26 @@ public class ListeForum extends HttpServlet {
      */
     public ListeForum() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		try {
 			request.setAttribute("forums", Forum.FindAll());
 			request.setAttribute("abonne", '0');
 			request.setAttribute("isadmin", request.getParameter("isadmin"));
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	    RequestDispatcher rd = request.getRequestDispatcher("AfficheForum.jsp");
 	    rd.forward(request, response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
 	}
 
 	/**
