ed532afb4623c1a896a1897bb610b016852dc719
Antoine Herkens <antoine.herkens@etu.utc.fr>
2020-06-07 17:45:33 +0200

    Update AddMessage.java

--- Devoir3propre/src/Controller/AddMessage.java
+++ Devoir3propre/src/Controller/AddMessage.java
@@ -28,40 +28,31 @@ public class AddMessage extends HttpServlet {
      */
     public AddMessage() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
-		System.out.println(session);
 		User u =(User) session.getAttribute("user");
-		System.out.println(session.getAttribute("user"));
 		int forumId= (int) session.getAttribute("forumId");
+		
 		try {
-			
-			System.out.println(forumId);
 			Forum forum= new Forum(forumId);
-			System.out.println(forum.getId());
+			
 			Message message = new Message();
 			message.setEditor(u);
 			message.setContent(request.getParameter("Text_message"));
-			//request.setAttribute("forums", Forum.addMessage(request.getParameter("Text_message"),u,forum));
 			forum.addMessage(message);
 			request.setAttribute("id", forumId);
-			System.out.println(forum.getId());
+			
 			forum.save();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(forumId);
