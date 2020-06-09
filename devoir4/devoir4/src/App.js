bce9bfbdc63faa2abfbd1621eddf23a14a24c956
Antoine Herkens <antoine.herkens@etu.utc.fr>
2020-06-09 18:56:02 +0200

    devoir 4 debut

---
+++ devoir4/devoir4/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './App.css';
+import Test from './components/Test.js';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      uvs: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://cors-anywhere.herokuapp.com/https://webapplis.utc.fr/Edt_ent_rest/myedt/result/?login=aherkens')
+        .then(res => res.json())
+        .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                uvs: result
+              });
+            },
+            // Remarque : il est important de traiter les erreurs ici
+            // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+            // des exceptions provenant de réels bugs du composant.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+        )
+  }
+
+  render() {
+    const listehtml = this.getuvliste();
+
+    return <div>
+      <Test uvs={this.state.uvs}/>
+      {listehtml}</div>;
+  }
+
+  getuvliste() {
+    const {error, isLoaded, uvs} = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+
+      return (
+          <div>
+            <h1> Liste des Uvs </h1>
+            <ul>
+              {uvs.map(item => (
+                  <li key={item.uv + item.type}>{item.uv}</li>
+              ))}
+
+            </ul>
+          </div>
+      );
+    }
+  }
+}
+export default App;
---
+++ devoir4/devoir4/src/components/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Test extends React.Component {
+
+    render() {
+        if(this.props.uvs.length !== 0){
+            console.log(this.props.uvs[0].day);
+            return <span>{this.props.uvs[0].day}</span>;
+        } return null;
+    }
+}
+
+export default Test;
