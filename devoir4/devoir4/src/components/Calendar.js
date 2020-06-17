8d031ebd915187f9bcf3d963581f2f7bb856d792
Antoine Herkens <antoine.herkens@etu.utc.fr>
2020-06-17 11:07:48 +0200

    calendrier semaine

--- devoir4/devoir4/src/components/Calendar.js
+++ devoir4/devoir4/src/components/Calendar.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import Timetable from 'react-timetable-events'
 import moment from 'moment';
 import Test from "../App";
 
 
+const days = {
+    "lundi": '2018-02-23T',
+    "mardi": '2018-02-24T',
+    "mercredi": '2018-02-25T',
+    "jeudi": '2018-02-26T',
+    "vendredi": '2018-02-27T'
+};
+
 class Calendar extends Component {
 
-    getInformation(stringify, day){
+
+    getInformation(stringify, day) {
         var uvsByDay = [];
         for (let i = 0; i < stringify.length; i++) {
-            console.log(i);
-            console.log(stringify[i]);
-            if (stringify[i].day.valueOf() == day.toString()){
-                console.log("get information : ", stringify[i].day);
+            //console.log(i);
+            //console.log(stringify[i]);
+            if (stringify[i].day.valueOf() == day.toString()) {
+                //console.log("get information : ", stringify[i].day);
                 uvsByDay.push(stringify[i]);
             }
         }
@@ -21,41 +30,73 @@ class Calendar extends Component {
         return uvsByDay;
     }
 
-    render () {
-        let json = JSON.stringify(this.props.uvs);
-        // console.log("JSON : ", json);
-        // console.log("JSON attribute : ", json["uv"]);
-        var stringify = JSON.parse(json);
+    formatByDay(uvsByDay, day) {
+        const uvsFormatees = [];
+        for (let i = 0; i < uvsByDay.length; i++) {
+            const coursI = {
+                name: uvsByDay[i].uv + "\n" + uvsByDay[i].room,
+                type: 'custom',
+                startTime: moment(day + uvsByDay[i].begin),
+                endTime: moment(day + uvsByDay[i].end)
+            }
+            uvsFormatees.push(coursI);
+        }
+        return uvsFormatees;
+    }
 
-        var days = {"lundi":'2018-02-23T', "mardi":'2018-02-24T', "mercredi":'2018-02-25T', "jeudi":'2018-02-26T', "vendredi":'2018-02-27T'};
+    render() {
+        if (this.props.uvs.length) {
+            let json = JSON.stringify(this.props.uvs);
+            // console.log("JSON : ", json);
+            // console.log("JSON attribute : ", json["uv"]);
+            const stringify = JSON.parse(json);
 
-        let coursLundi = this.getInformation(stringify, "LUNDI");
-        let coursMardi = this.getInformation(stringify, "MARDI");
-        let coursMercredi = this.getInformation(stringify, "MERCREDI");
-        let coursJeudi = this.getInformation(stringify, "JEUDI");
-        let coursVendredi = this.getInformation(stringify, "VENDREDI");
+            const coursLundi = this.getInformation(stringify, "LUNDI");
+            let coursMardi = this.getInformation(stringify, "MARDI");
+            let coursMercredi = this.getInformation(stringify, "MERCREDI");
+            let coursJeudi = this.getInformation(stringify, "JEUDI");
+            let coursVendredi = this.getInformation(stringify, "VENDREDI");
 
+
+            this.state = {
+                events: {
+                    monday: this.formatByDay(coursLundi, days.lundi),
+                    tuesday: this.formatByDay(coursMardi, days.mardi),
+                    wednesday: this.formatByDay(coursMercredi, days.mercredi),
+                    thursday: this.formatByDay(coursJeudi, days.jeudi),
+                    friday: this.formatByDay(coursVendredi, days.vendredi)
+                }
+            };
+
+            // this.state = {
+            //     events: {
+            //         monday: [
+            //             {
+            //                 name: coursLundi[0].uv + "\n" + coursLundi[0].room,
+            //                 type: 'custom',
+            //                 startTime: moment(days.lundi + coursLundi[0].begin),
+            //                 endTime: moment(days.lundi + coursLundi[0].end)
+            //             }
+            //         ]
+            //     }
+            // };
+            return <Timetable events={this.state.events}/>
+        }
 
         this.state = {
             events: {
-                monday :[
+                monday: [
                     {
-                        //id: 1,
-                        //name: this.getInformation(stringify, "uv"),
-                        name : 'coursLundi[0].day',
+                        name: 'test',
                         type: 'custom',
                         startTime: moment('2018-02-23T11:30:00'),
-                        //startTime: moment(days.lundi + this.getInformation(stringify, "uv")),
-                        //endTime: moment('2018-02-23T13:30:00')
                         endTime: moment('2018-02-23T13:30:00')
                     }
                 ]
             }
         };
-
-       // console.log(this.props.uvs);
-        console.log("stringify", stringify);
-
+        // console.log(this.props.uvs);
+        //console.log("stringify", stringify);
 
 
         // var obj = '[{"availability_id":"109465","date":"2017-02-21","price":"430000"},{"availability_id":"109466","date":"2017-02-22","price":"50000000"},{"availability_id":"109467","date":"2017-02-23","price":"6666666666"}]';
@@ -66,10 +107,10 @@ class Calendar extends Component {
         //console.log(this.state.uvs.events[0]);
         //this.setState({events : this.props.uvs});
         return <Timetable events={this.state.events}/>
-
     }
 
-
+    // TODO : check variables
+    // TODO : salle dans propriété à part
 }
 
 export default Calendar;
